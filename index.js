f2936f0e95d6f3d8d34bda33907027f30fa110df
vn50fsa <Rodolfo.Saldivar.Hernandez@walmart.com>
2019-12-16 14:21:40 -0600

    Update index.js

--- index.js
+++ index.js
@@ -51,7 +51,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static('todo-pwa/build'));
   const path = require('path');
   app.get('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../todo-pwa', 'build', 'index.html'));
+    res.sendFile(path.resolve(__dirname, './todo-pwa', 'build', 'index.html'));
   });
 }
 
