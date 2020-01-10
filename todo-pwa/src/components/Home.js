661a2939b6cd1d8fd1a155988178bd1a2ffa4c97
vn50fsa <Rodolfo.Saldivar.Hernandez@walmart.com>
2020-01-10 10:41:30 -0600

    both

--- todo-pwa/src/components/Home.js
+++ todo-pwa/src/components/Home.js
@@ -1,50 +1,18 @@
-import React, { Component } from 'react'
-import QrReader from 'react-qr-scanner'
- 
-export default class QRScanner extends Component {
-  constructor(props){
-    super(props)
-    this.state = {
-      delay: 500,
-      result: 'No result',
-    }
- 
-    this.handleScan = this.handleScan.bind(this)
-  }
-  handleScan(data){
-    this.setState({
-      result: data,
-    });
-    console.log(data);
-  }
-  handleError(err){
-    console.error(err)
-  }
-  render(){
-    const previewStyle = {
-      height: 240,
-      width: 320,
-    }
- 
-    return(
-      <div>
-        <QrReader
-          delay={this.state.delay}
-          style={previewStyle}
-          onError={this.handleError}
-          onScan={this.handleScan}
-          />
-          -----------------------------------------------------------------
-        {/*facingMode='environment'
-                <QrReader
-                  delay={this.state.delay}
-                  style={previewStyle}
-                  onError={this.handleError}
-                  onScan={this.handleScan}
-                  facingMode='environment'
-                  />*/}
-        <p>{this.state.result}</p>
-      </div>
-    )
-  }
-}
+import React from 'react';
+import Normal from './Normal';
+import Legacy from './Legacy';
+
+const Home = () => {
+	return (
+		<div>
+			Normal:
+			<Normal />
+			------------------------------------------------------
+			<br />
+			Legacy:
+			<Legacy />
+		</div>
+	);
+};
+
+export default Home;
